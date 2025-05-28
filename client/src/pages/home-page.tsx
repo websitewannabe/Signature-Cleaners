2f0d04c0a4f56414ab7b492baa950c6a29dcc5bc
originalwannabe <ctierney@websitewannabe.com>
2025-05-28 19:45:20 +0000

    Restored to '937d883b2874afcbb07e9ddbffe383a33a2d8757'
    Replit-Restored-To: 937d883b2874afcbb07e9ddbffe383a33a2d8757

--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "wouter";
-import { Car, Search, Shirt } from "lucide-react";
+import { Shirt, Car, Search } from "lucide-react";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 import { Button } from "@/components/ui/button";
